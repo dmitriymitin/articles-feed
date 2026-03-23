22c6ef4be8d47f3f14216122bdcb483c7f125b33
dmitriymitin <118613405+dmitriymitin@users.noreply.github.com>
2026-03-23 11:29:47 +0400

    fix storybook

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import {Trans} from "shared/ui/Translate";
-import {Counter} from "entities/Counter";
 
 const AboutPage = () => {
   return (
     <div>
       <Trans ns='about'>О сайте</Trans>
-      <Counter />
     </div>
   );
 };
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import {Trans} from "shared/ui/Translate";
-import {Counter} from "entities/Counter";
 
 
 const MainPage = () => {
   return (
     <div>
       <Trans>Главная страница</Trans>
-      <Counter />
     </div>
   );
 };
