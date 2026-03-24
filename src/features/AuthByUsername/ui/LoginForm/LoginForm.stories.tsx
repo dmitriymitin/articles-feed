b9ab86983ea5684474e29cd7438cdf50a1df51fa
dmitriymitin <118613405+dmitriymitin@users.noreply.github.com>
2026-03-24 23:29:31 +0400

    update tests

---
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.stories.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import { LoginForm } from "./LoginForm";
+
+export default {
+  title: "features/LoginForm",
+  component: LoginForm,
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof LoginForm>;
+
+const Template: ComponentStory<typeof LoginForm> = (args) => (
+  <LoginForm {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {};
+Primary.decorators = [];
+
+export const withError = Template.bind({});
+withError.args = {};
+withError.decorators = [];
+
+export const Loading = Template.bind({});
+Loading.args = {};
+Loading.decorators = [];
