b2d1e2769de0773883bb804cc99f9c94cf1cfc14
dmitriymitin <118613405+dmitriymitin@users.noreply.github.com>
2026-03-26 01:53:50 +0400

    fix types

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -1,18 +1,27 @@
-import React, {PropsWithChildren, useMemo, useState} from 'react'
-import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext, ThemeContextProps} from '../lib/ThemeContext';
+import React, { PropsWithChildren, useMemo, useState } from "react";
+import {
+  LOCAL_STORAGE_THEME_KEY,
+  Theme,
+  ThemeContext,
+  ThemeContextProps,
+} from "../lib/ThemeContext";
 
-const defaultTheme = localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme || Theme.LIGHT
+const defaultTheme =
+  (localStorage.getItem(LOCAL_STORAGE_THEME_KEY) as Theme) || Theme.LIGHT;
 
 interface ThemeProviderProps extends PropsWithChildren {
-  initialTheme?: Theme
+  initialTheme?: Theme;
 }
 
 export const ThemeProvider = (props: ThemeProviderProps) => {
-  const { initialTheme, children } = props
+  const { initialTheme, children } = props;
 
-  const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme)
+  const [theme, setTheme] = useState<Theme>(initialTheme || defaultTheme);
 
-  const defaultProps: ThemeContextProps = useMemo(() => ({ theme, setTheme }), [theme])
+  const defaultProps: ThemeContextProps = useMemo(
+    () => ({ theme, setTheme }),
+    [theme]
+  );
 
   return (
     <ThemeContext.Provider value={defaultProps}>
