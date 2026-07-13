34bb8ca0711f763058de213b702126de2c512d32
Dmitriy Mitin <118613405+dmitriymitin@users.noreply.github.com>
2026-07-13 14:37:35 +0400

    add new TODO

--- src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.tsx
+++ src/pages/ArticleEditPage/ui/ArticleEditPage/ArticleEditPage.tsx
@@ -1,25 +1,25 @@
-import { useTranslation } from 'react-i18next';
-import { useParams } from 'react-router-dom';
+import { useTranslation } from "react-i18next";
+import { useParams } from "react-router-dom";
 
-import { Page } from '@/widgets/Page';
+import { Page } from "@/widgets/Page";
 
-import s from './ArticleEditPage.module.scss';
+import s from "./ArticleEditPage.module.scss";
 
-
+// TODO: [feature] - добавить страницу редактирования статей
 const ArticleEditPage = () => {
-    const { t } = useTranslation();
+  const { t } = useTranslation();
 
-    const { id } = useParams<{ id: string }>();
+  const { id } = useParams<{ id: string }>();
 
-    const isEdit = Boolean(id);
+  const isEdit = Boolean(id);
 
-    return (
-      <Page className={s.ArticleEditPage}>
-          {isEdit
-            ? t('Редактирование статьи с ID = ') + id
-            : t('Создание новой статьи')}
-      </Page>
-    );
+  return (
+    <Page className={s.ArticleEditPage}>
+      {isEdit
+        ? t("Редактирование статьи с ID = ") + id
+        : t("Создание новой статьи")}
+    </Page>
+  );
 };
 
 export default ArticleEditPage;
